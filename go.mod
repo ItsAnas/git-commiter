9f57f10ff8243bddd76784895f75d3c6452b7cd4
ItsAnas <97anas@gmail.com>
2020-05-18 18:26:34 +0200

    "feat(git): ask the user if he wants to commit"

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ItsAnas/git-commit-configurator
 
 go 1.14
+
+require github.com/manifoldco/promptui v0.7.0
--- main.go
+++ main.go
@@ -2,16 +2,43 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
+
+	"github.com/manifoldco/promptui"
 )
 
-func main() {
-	fmt.Println("Hello, world.")
-	myArguments := []string{"branch", "--all"}
+func askForCommit() bool {
+	prompt := promptui.Select{
+		Label: "Do you want to commit ? [Yes/No]",
+		Items: []string{"Yes", "No"},
+	}
+	_, result, err := prompt.Run()
+	if err != nil {
+		log.Fatalf("Prompt failed %v\n", err)
+	}
+	return result == "Yes"
+}
+
+func parseCommitMessage(input string) string {
+	return fmt.Sprintf("\"feat(git): %s\"", input)
+}
+
+func commitMessage(input string) bool {
+	formattedMessage := parseCommitMessage(input)
+	myArguments := []string{"commit", "-m", formattedMessage}
 	output, err := exec.Command("git", myArguments...).CombinedOutput()
 	if err != nil {
 		os.Stderr.WriteString(err.Error())
+		return false
 	}
 	fmt.Println(string(output))
+	return true
+}
+
+func main() {
+	if askForCommit() {
+		commitMessage("ask the user if he wants to commit")
+	}
 }
